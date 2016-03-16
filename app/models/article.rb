62a99f72942eb7c35efca582714e561681f59281
Joel Uzibor <josamuyi@yahoo.com>
2016-03-16 15:39:50 +0000

    Add association between user and articles, add deburgger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  belongs_to :user 
   validates :title, presence: true, length: { minimum: 3, maximum: 40 } 
-  validates :description, presence: true, length: { minimum: 20, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 500000 }
+  validates :user_id, presence: true
 end
