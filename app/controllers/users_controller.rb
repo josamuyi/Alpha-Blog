9ecfa51d5a0824074238a6ec14e784f81bc6ca23
Joel Uzibor <josamuyi@yahoo.com>
2016-03-17 22:42:15 +0000

    Add sign in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Joel's Alpha Blog Site"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
