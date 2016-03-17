72c11952bd5d0b0a17b74e7c1b4cf6eb324bbb47
Joel Uzibor <josamuyi@yahoo.com>
2016-03-17 01:21:38 +0000

    Added user signup form and logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'pages#home'
